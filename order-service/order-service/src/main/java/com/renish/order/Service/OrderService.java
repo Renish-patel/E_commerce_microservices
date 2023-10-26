c7de475034306918c602757d533a74e7f785c786
Renishavaiya <renish.avaiya@swarcklabs.com>
2023-10-26 19:01:06 +0530

    added

--- order-service/order-service/src/main/java/com/renish/order/Service/OrderService.java
+++ order-service/order-service/src/main/java/com/renish/order/Service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
 	private final OrderRepository orderRepository;
-	private final WebClient webClient;
+	private final WebClient.Builder	 webClientBuilder;
 
 	public void placeOrder(OrderRequest orderRequest) {
 		Order order = new Order();
@@ -34,7 +34,9 @@ public class OrderService {
 		
 		// call inventoy service to see wheather the product is in stock or not
 		
-		InventoryResponse[] inventoryResponseArray = webClient.get().uri("http://localhost:8083/inventory",
+		InventoryResponse[] inventoryResponseArray = webClientBuilder.build()
+				.get()
+				.uri("http://INVENTORY-SERVICE/inventory",
 				UriBuilder -> UriBuilder.queryParam("skuCode", skuCodes).build())
 				.retrieve().bodyToMono(InventoryResponse[].class)
 				.block();
